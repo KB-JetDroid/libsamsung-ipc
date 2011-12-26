5cb9e6929df03d5c09f8cc6aaea8cd7b13b41f0a
KB <kbjetdroid@gmail.com>
2011-12-26 22:35:00 +0530

    Restored ipc_client_free while exiting the loop

--- tools/modemctrl.c
+++ tools/modemctrl.c
@@ -4,6 +4,8 @@
  * Copyright (C) 2010-2011 Joerie de Gram <j.de.gram@gmail.com>
  *               2011 Simon Busch <morphis@gravedo.de>
  *
+ * Modified for Jet - KB <kbjetdroid@gmail.com>
+ *
  * libsamsung-ipc is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -582,7 +584,7 @@ int main(int argc, char *argv[])
         }
 
 modem_quit:
-//    ipc_client_free(client_fmt);
+    ipc_client_free(client_fmt);
 
     return 0;
 }
